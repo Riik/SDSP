62b253fc0a4b26629f0f556dba2dc0bc049742ac
Riik <rikvandervlist@gmail.com>
2016-10-19 17:43:25 +0200

    Autocorrelation function and matrix working properly

--- python/lpc.py
+++ python/lpc.py
@@ -5,77 +5,47 @@ Created on Tue Oct 18 17:01:14 2016
 @author: Rik van der Vlist
 """
 from scipy.io import wavfile
-import math
 import numpy as np
-import matplotlib.pyplot as plt
-
-def acf(series):
-    n = len(series)
-    data = np.asarray(series)
-    mean = np.mean(data)
-    c0 = np.sum((data) ** 2) 
-    print("C0")
-    print(c0)
-    print(np.dot(frame, frame))
-
-    def r(h):
-        acf_lag = ((data[:n - h] ) * (data[h:] )).sum() / float(n) / c0
-        return round(acf_lag, 3)
-    x = np.arange(n) # Avoiding lag 0 calculation
-    acf_coeffs = map(r, x)
-    return acf_coeffs
-
 
 [fs, wavdata] = wavfile.read('clean.wav')
-wavfile.write('cleanloud.wav', fs, wavdata*3)
+
 #define filter order p
 p = 5;
 
-WINDOW_LEN_MS = 1
+WINDOW_LEN_MS = 20
 N = fs * (WINDOW_LEN_MS) // 1000
 
 Lk = 200000
 frame = wavdata[Lk:Lk+N]
-#####
-## METHOD 0
-#####
-coeff = acf(frame)
-
-
-
+frame = np.ndarray.astype(frame, 'int32')
 ######
-## METHOD 1
+## Estimate autocorrelation
 ######
 acf = np.array([0]*p)
+# calculate first term by sum of x*x_H
 acf[0] = np.dot(frame, frame)
 for k in range(1,p):
+    # create k_th term by creating to vectors with offset k and length N-k
     acf[k] = (frame[k:] * frame[:-k]).sum() 
+# divide by N, a biased estimate is obtained
 acf = acf / N
-
-######
-## METHOD 2
-######
-# create autocorrelation array from r[-N/2] to r[N/2]. r[0] is in the center
-autocorrelation = np.correlate(frame, frame, "full");
-mid = math.floor(len(autocorrelation)/2)
-
-#transfrom to multidimensional array
-autocorrelation = autocorrelation.reshape(-1,1)
-# normalize by N
-#autocorrelation = autocorrelation / window_len
-plt.plot(autocorrelation)
-#plt.show()
-R = autocorrelation[mid:mid+p]
+# make the autocorrelation symmetric from -p to p and convert from N array to Nx1 array
+acf = np.concatenate([acf[p:0:-1], acf])
+acf = acf.reshape(-1,1)
+# compose autocorrelation matrix from the autocorrelation sequence
+R = acf[-p:]
 for i in range(1,p):
-    R = np.hstack((R, autocorrelation[mid-i:mid-i+p]))
-
-wavfile.write('cleanloud.wav', fs, frame)
-#frame = np.reshape(frame, (-1,1))
-#print(R)
-#print("eigenvalues:")
-#print(np.linalg.eig(R))
+    R = np.hstack((R, acf[-p-i:-i]))
 
 #compare methods 
-print(autocorrelation[mid:mid+p])
+print('Autocorrelation function from -p to p')
 print(acf)
-print([x for x in coeff])
+print('Autocorrelation matrix')
+print(R);
+[eig, eigvect] = np.linalg.eig(R)
+print('Eigenvalues autocorrelation matrix')
+print(eig)
+
+wavfile.write('cleanloud.wav', fs, frame)
+
+
