80c94cf450ea9df4dd131698f73ab0e6e4d202c2
Riik <rikvandervlist@gmail.com>
2016-10-19 17:44:41 +0200

    Added comments to lpc.py

--- python/lpc.py
+++ python/lpc.py
@@ -12,17 +12,21 @@ import numpy as np
 #define filter order p
 p = 5;
 
+# calculate frame length in samples from length in ms
 WINDOW_LEN_MS = 20
 N = fs * (WINDOW_LEN_MS) // 1000
 
+# frame offset
 Lk = 200000
+# get a frame of length N with starting point Lk from data 
 frame = wavdata[Lk:Lk+N]
+# At this part some windowing is probably needed
+# Now we convert from int16 to int32 to avoid compatibility issues in Python
 frame = np.ndarray.astype(frame, 'int32')
-######
-## Estimate autocorrelation
-######
+# next step is estimating the autocorrelation
+# initialize the autocorrelation function with zeros
 acf = np.array([0]*p)
-# calculate first term by sum of x*x_H
+# calculate first term by x*x_H (sum of elementwise squares)
 acf[0] = np.dot(frame, frame)
 for k in range(1,p):
     # create k_th term by creating to vectors with offset k and length N-k
@@ -33,11 +37,11 @@ acf = acf / N
 acf = np.concatenate([acf[p:0:-1], acf])
 acf = acf.reshape(-1,1)
 # compose autocorrelation matrix from the autocorrelation sequence
+# a shifted version of the function is added to the matrix column per column
 R = acf[-p:]
 for i in range(1,p):
     R = np.hstack((R, acf[-p-i:-i]))
 
-#compare methods 
 print('Autocorrelation function from -p to p')
 print(acf)
 print('Autocorrelation matrix')
@@ -46,6 +50,6 @@ print(R);
 print('Eigenvalues autocorrelation matrix')
 print(eig)
 
-wavfile.write('cleanloud.wav', fs, frame)
+#wavfile.write('cleanloud.wav', fs, frame)
 
 
